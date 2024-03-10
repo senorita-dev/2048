08ab6ff21be414888152b80e72a6ec6a47c95e69
Ren Saito <ren.saito.dev@gmail.com>
2024-03-10 21:06:06 +1300

    basic layout started

---
+++ src/components/Grid.tsx
@@ -0,0 +1,24 @@
+import "../css/Grid.css";
+
+const Grid = () => {
+  return <div className="grid">
+		<div className="cell">1</div>
+		<div className="cell">2</div>
+		<div className="cell">3</div>
+		<div className="cell">4</div>
+		<div className="cell">5</div>
+		<div className="cell">6</div>
+		<div className="cell">7</div>
+		<div className="cell">8</div>
+		<div className="cell">9</div>
+		<div className="cell">10</div>
+		<div className="cell">11</div>
+		<div className="cell">12</div>
+		<div className="cell">13</div>
+		<div className="cell">14</div>
+		<div className="cell">15</div>
+		<div className="cell">16</div>
+	</div>;
+};
+
+export default Grid;
---
+++ src/components/NewGameButton.tsx
@@ -0,0 +1,5 @@
+const NewGameButton = () => {
+  return <button>New Game</button>;
+};
+
+export default NewGameButton;
